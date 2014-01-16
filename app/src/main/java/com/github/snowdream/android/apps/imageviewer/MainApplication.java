5236bea4770f08ec0f134db32c4a3055baf9f37f
snowdream <yanghui1986527@gmail.com>
2014-01-16 15:56:19 +0800

    [Add] Initial Branch

---
+++ app/src/main/java/com/github/snowdream/android/apps/imageviewer/MainApplication.java
@@ -0,0 +1,8 @@
+package com.github.snowdream.android.apps.imageviewer;
+
+/**
+ * Created by yanghui07 on 14-1-16.
+ */
+public class MainApplication extends Application {
+
+}
---
+++ lib/src/main/java/com/github/snowdream/android/helloworld/HelloWorldLib.java
@@ -0,0 +1,11 @@
+package com.github.snowdream.android.helloworld;
+
+/**
+ * Created by snowdream on 11/17/13.
+ */
+public class HelloWorldLib {
+
+    public static String getHelloWorld(){
+        return "Hello World From HelloWorldLib!";
+    }
+}
