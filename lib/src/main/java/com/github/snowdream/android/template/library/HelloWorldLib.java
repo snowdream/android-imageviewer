fccda93e507d592826fda02f2cb366932b752b0a
snowdream <yanghui1986527@gmail.com>
2014-05-16 23:35:44 +0800

    Added: PACKAGE_NAME

--- lib/src/main/java/com/github/snowdream/android/template/library/HelloWorldLib.java
+++ lib/src/main/java/com/github/snowdream/android/template/library/HelloWorldLib.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.github.snowdream.android.helloworld;
+package com.github.snowdream.android.template.library;
 
 /**
  * Created by snowdream on 11/17/13.
